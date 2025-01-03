af41160afed328ee36cf593607c152e3cd093be0
Tomaz Pontes <tomazmppontes@gmail.com>
2025-01-02 21:24:24 -0300

    feat: video explanation feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Header from './components/landing-page/Header';
 import Hero from './components/landing-page/Hero';
+import VideoExplanation from './components/landing-page/video-explanation';
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
-     {/*  
       <VideoExplanation />
+     {/*  
+      
       <Pricing />
       <FAQ /> */}
     </div>
