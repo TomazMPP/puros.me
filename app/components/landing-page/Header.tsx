8643f4c2453b09ad3850192a9e4d5bc65f1e8670
Tomaz Pontes <tomazmppontes@gmail.com>
2025-01-03 02:08:43 -0300

    refactor: lib folder

--- app/components/landing-page/Header.tsx
+++ app/components/landing-page/Header.tsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
 import Button from "../ui/Button";
+import { auth } from "@/app/lib/auth";
+import { manageAuth } from "@/app/actions/ManageAuth";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+  
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
+        <Link href="/">
         <div className="flex items-center gap-4">
-        <img src="/logo.svg" alt="Puros Logo" />
-        <h3 className="text-white text-2xl font-bold">Puros.me</h3>
+          <img src="/logo.svg" alt="Puros Logo" />
+          <h3 className="text-white text-2xl font-bold">Puros.me</h3>
         </div>
+        </Link>
         <div className="flex items-center gap-4">
-          <Button>My Page</Button>
-          <Button>Log Out</Button>
+        {session && <Button>Minha Página</Button>}
+        <form action={manageAuth}>
+          <Button>{session ? "Sair" : "Login"}</Button>
+        </form>
+
+          
         </div>
     </div>
   )
