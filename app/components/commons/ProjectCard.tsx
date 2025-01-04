1040032be03a9600559a760aac217753ee0e5161
Tomaz Pontes <tomazmppontes@gmail.com>
2025-01-04 02:16:42 -0300

    feat: project total clicks

--- app/components/commons/ProjectCard.tsx
+++ app/components/commons/ProjectCard.tsx
@@ -1,23 +1,28 @@
 "use client"
 
+import { increaseProjectVisits } from "@/app/actions/IncreaseProjectVisits";
 import { formatUrl } from "@/app/lib/utils";
 import { ProjectData } from "@/app/server/GetProfileData";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export default function ProjectCard({
   project, isOwner, img,
 }: {
-  project: ProjectData,
+  project?: ProjectData,
   isOwner: boolean, 
   img: string,
 }) {
 
-  const projectUrl = formatUrl(project.projectUrl) 
+  const { profileId } = useParams()
+  const projectUrl = formatUrl(project?.projectUrl || "");
   
-
-  function handleClick() {
-    console.log("clicked") // TODO: analytics
+  async function handleClick() {
+    if (!profileId || !project?.id) return
+    await increaseProjectVisits(profileId as string, project.id)
   }
+
+  
   return (
     <Link href={projectUrl} target="_blank" onClick={handleClick}>
     <div className="w-[340px] min-h-[132px] max-h-96 flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
@@ -27,13 +32,13 @@ export default function ProjectCard({
       <div className="flex flex-col gap-2">
         {
           isOwner && <span className="uppercase text-xs font-bold text-accent-green">
-          {project.totalVisits || 0} Clicks
+           {project?.totalVisits || 0} Clicks
         </span>
         }
         
         <div className="flex flex-col">
-        <span className="text-white font-bold text-xl">{project.projectName}</span>
-        <span className="text-content-body text-sm">{project.projectDescription}</span>
+        <span className="text-white font-bold text-xl">{project?.projectName}</span>
+        <span className="text-content-body text-sm">{project?.projectDescription}</span>
         </div>
       </div>
     </div>
