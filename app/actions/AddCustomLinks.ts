c3dcb9877d9e259cec2b64f2bca27f7206a4bfe8
Tomaz Pontes <tomazmppontes@gmail.com>
2025-01-04 03:03:05 -0300

    fix: create custom link db

--- app/actions/AddCustomLinks.ts
+++ app/actions/AddCustomLinks.ts
@@ -24,11 +24,22 @@ export default async function addCustomLinks({
   if (!session) return false
   
   try {
-    await db.collection("profiles").doc(profileId).update({
-      link1,
-      link2,
-      link3,
-    })
+    const linksToUpdate: { [key: string]: Link } = {}
+    
+    if (link1?.title && link1?.url) {
+      linksToUpdate.link1 = link1
+    }
+    
+    if (link2?.title && link2?.url) {
+      linksToUpdate.link2 = link2
+    }
+    
+    if (link3?.title && link3?.url) {
+      linksToUpdate.link3 = link3
+    }
+
+    await db.collection("profiles").doc(profileId).update(linksToUpdate)
+    return true
   } catch (error) {
     console.error("Error adding custom links: ", error)
     return false
